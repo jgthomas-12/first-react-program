b310ba5eeadb6a8995f01b6d80d451e2ef23bbc7
Jesse Thomas <jgthomas3@gmail.com>
2023-12-18 21:18:20 -0700

    Add Header and Counter components to work in the app

---
+++ src/Components/Counter.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useState } from "react";
+import "../Counter.css";
+
+function Counter() {
+  let count = 0;
+  const [currentstate, updatedstate] = useState(count);
+  const handleclick = () => {
+    updatedstate(currentstate + 1);
+  };
+
+  return(
+    <>
+      <div className="counter_parent">
+        <div className="main-div">
+          <h3>Counter</h3>
+          <button onClick={handleclick}>{currentstate}</button>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Counter;
